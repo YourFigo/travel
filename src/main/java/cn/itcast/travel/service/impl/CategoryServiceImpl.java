9789e6bc22dca2a35b17d2bdbf8f1e815af909ad
YourFigo <501385003@qq.com>
2019-10-21 23:22:59 +0800

    将分类信息导航查询改为redis缓存

--- src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -19,8 +19,41 @@ public class CategoryServiceImpl implements CategoryService {
 
     private CategoryDao categoryDao = new CategoryDaoImpl();
 
+    /**
+     * 由于这个数据基本不变，为了减少访问数据库的开销，使用redis缓存
+     * @return
+     */
     @Override
     public List<Category> findAll() {
-        return categoryDao.findAll();
+        //1.从redis中查询
+        //1.1获取jedis客户端
+        Jedis jedis = JedisUtil.getJedis();
+        //1.2可使用sortedset排序查询
+        Set<String> categorys = jedis.zrange("category", 0, -1);
+
+        List<Category> cs = null;
+        //2.判断查询的集合是否为空
+        if (categorys == null || categorys.size() == 0) {
+            System.out.println("分类信息 从数据库查询....");
+            //3.如果为空,需要从数据库查询,在将数据存入redis
+            //3.1 从数据库查询
+            cs = categoryDao.findAll();
+            //3.2 将集合数据存储到redis中的 category的key
+            for (int i = 0; i < cs.size(); i++) {
+                //让 sortedset 按id号排序
+                jedis.zadd("category", cs.get(i).getCid(), cs.get(i).getCname());
+            }
+        } else {
+            System.out.println("分类信息 从redis中查询.....");
+            //4.如果不为空,将从redis取出的set<String>数据存入list<Category>
+            cs = new ArrayList<Category>();
+            for (String name : categorys) {
+                Category category = new Category();
+                category.setCname(name);
+                cs.add(category);
+            }
+        }
+        return cs;
     }
+
 }
